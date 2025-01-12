eb44223562c0480f265ad2d53c92d399c7038fc9
Mohd Haji <mohdhaji1717@gmail.com>
2025-01-12 20:16:18 +0400

    home page display fix when site load

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
     <div>
       <Router>
         <Navbar />
+        <Home />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/privacy" element={<Privacy />} />
